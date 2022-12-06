5db79c8f2a3616972c898d7bb258657a9942ed77
Vinh Nguyen <nguyenlebaovinh.27@gmail.com>
2022-12-06 13:15:33 -0600

    commit tree c4919e64689dbd8cf8c10f4edfa4ff515db20f53

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(name = ["Guadalupe", "Ollie", "Aki"], event = "surprise")
+{
+    let newArray =[];
+    for(let i = 0; i < name.length; i++)
+    {
+        newArray[i] = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+    }
+    return newArray;
+}
+
+function countDown(number)
+{
+    let i = number;
+    while (i >= 0)
+    {
+        console.log(i);
+        i--;
+    }
+    return i;
+}
